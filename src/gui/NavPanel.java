ab0c9c5c76d2e0c49e89202e2a0d0ea82df385fd
vpativada <vpativada@dmacc.edu>
2018-05-02 22:33:32 -0500

    Code Clean Up

--- src/gui/NavPanel.java
+++ src/gui/NavPanel.java
@@ -1,4 +1,5 @@
 package gui;
+
 /**
  * @author Vasavi Pativada
 */
@@ -13,28 +14,21 @@ public class NavPanel extends JPanel {
 	private static final long serialVersionUID = 657242257303212071L;
 	private JPanel NavPanel;
 	private JButton btnStore;
-	private JButton btnInventory;
-	private JButton btnFinance; 
 	private JButton btnCheckout;
-    private JLabel welcome;
+	private JLabel welcome;
 
-
-	public NavPanel(String accountTypeString) { 
+	public NavPanel(String accountTypeString) {
 		NavPanel = new JPanel();
 
 		welcome = new JLabel("");
 		NavPanel.add(welcome);
-		if(accountTypeString.equals("Admin")){
-			createAdminButtons();
-		}else{
-			createUserButtons();
-		}
+		createUserButtons();
 	}
 
 	public void addStoreListener(ActionListener listener) {
 		btnStore.addActionListener(listener);
 	}
-	
+
 	public void addCartListener(ActionListener listener) {
 		btnCheckout.addActionListener(listener);
 	}
@@ -43,21 +37,7 @@ public class NavPanel extends JPanel {
 		return NavPanel;
 	}
 
-	public void createAdminButtons(){
-		btnInventory = new JButton("Inventory");
-		NavPanel.add(btnInventory);
-		
-		btnFinance = new JButton("Finance");
-		NavPanel.add(btnFinance);
-
-		btnStore = new JButton("Store");
-		NavPanel.add(btnStore);
-		
-		btnCheckout = new JButton("Shopping Cart");
-		NavPanel.add(btnCheckout);
-	}
-	
-	public void createUserButtons(){
+	public void createUserButtons() {
 		btnStore = new JButton("Store");
 		NavPanel.add(btnStore);
 
